a9973adc3b737f65049f196ad36a2e8e0ea684b9
Valkyria <valkyria.fsilva@gmail.com>
2022-01-19 21:57:34 -0300

    commit update

--- api/api/urls.py
+++ api/api/urls.py
@@ -2,7 +2,6 @@ from rest_framework import routers
 from posts.views import PostViewSet
 from django.contrib import admin
 from django.urls import path, include
-from users.views import CreateUserView
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -12,6 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('followers.urls')),
-    path ('api-auth/',  include ('rest_framework.urls')),
-    path('api-auth/registration/', CreateUserView.as_view()), 
+    path ('api-auth/',  include('rest_framework.urls')),
+    path('api-auth/', include('users.urls')), 
 ]
